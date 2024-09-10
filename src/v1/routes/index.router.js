c72b8b0a844be11b4d43b0a6ec6f7c1db8446baa
HieuIN <hieuin17021995@gmail.com>
2024-09-11 01:07:21 +0700

    Add api

--- src/v1/routes/index.router.js
+++ src/v1/routes/index.router.js
@@ -8,4 +8,25 @@ router.get('/checkstatus', (req, res, next) => {
     })
 })
 
+router.get('/api/user', (req, res, next) => {
+    res.status(200).json({
+        status: 'success',
+        message: 'api ok',
+        metadata:[
+        {
+            name:'antony sprin',
+            age: 25
+        },
+        {
+            name:'Ronaldo',
+            age: 40
+        },
+        {
+            name:'Messi',
+            age: 37
+        }
+        ]
+    })
+})
+
 module.exports = router;
